6f7dcb7ef11b9bb41eaa96afa6ae88a3f27a53a3
Q2Computing <83981717+Q2Computing@users.noreply.github.com>
2024-03-13 10:02:29 -0400

    Update entrypoint.sh
    Setting up a vector database using GitHub actions. I need a test to confirm the vector database is operational.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
+docker rm -f marqo
+docker pull marqoai/marqo:latest
+docker run --name marqo -p 8882:8882 marqoai/marqo:latest
+
+sudo apt update
+sudo apt install python3-pip -y
+pip install marqo

dd820a63ebe4b3fb0f3e06db4f9068bee2134e55
Q2Computing <83981717+Q2Computing@users.noreply.github.com>
2024-03-16 15:27:44 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,3 @@
 #!/bin/sh -l
 
-docker rm -f marqo
-docker pull marqoai/marqo:latest
-docker run --name marqo -p 8882:8882 marqoai/marqo:latest
-
-sudo apt update
-sudo apt install python3-pip -y
-pip install marqo
-
-python vector.py
+python transformer.py

d866ee293868066baea59fcca03d1d57fc4fffec
Q2Computing <83981717+Q2Computing@users.noreply.github.com>
2024-04-02 10:17:20 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh -l
-try:
-  pip install -r requirements.txt
-except error as e:
-  echo e
+pip install -r requirements.txtexcept error as e:
+
 python train.py --batch_size=32 --compile=False

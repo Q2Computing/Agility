203a712fcf113b728cafafed9d6aafaede50d2e8
Q2Computing <83981717+Q2Computing@users.noreply.github.com>
2024-04-02 08:00:52 -0400

    Update train.py
    Setting the manual seed for torch so that the system is reproducible

--- train.py
+++ train.py
@@ -26,6 +26,7 @@ import numpy as np
 import torch
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed import init_process_group, destroy_process_group
+torch.manual_seed(1337)
 
 from model import GPTConfig, GPT
 

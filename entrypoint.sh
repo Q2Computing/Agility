f8dc175bf06985e9ca7a99e5447c00d753afd6f0
Q2Computing <83981717+Q2Computing@users.noreply.github.com>
2024-04-02 10:31:47 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,2 @@
 #!/bin/sh -l
-sudo apt update
-sudo apt install python3
-python -m pip install venv
-pip install -r requirements.txt
-python train.py --batch_size=32 --compile=False
+echo "Hello World!"
